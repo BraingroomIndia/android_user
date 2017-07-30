3336bc84fc3227c61785eea8b71f052d735ffe4a
Vikas Godara <viki.godara@gmail.com>
2017-07-30 19:03:53 +0530

    Before version 2

---
+++ app/src/main/java/com/braingroom/user/utils/SplashViewPager.java
@@ -0,0 +1,10 @@
+package com.braingroom.user.utils;
+
+import android.support.v4.view.PagerAdapter;
+
+/**
+ * Created by godara on 30/07/17.
+ */
+
+public class SplashViewPager extends PagerAdapter {
+}
---
+++ app/src/main/java/com/braingroom/user/viewmodel/SplashViewModel.java
@@ -0,0 +1,8 @@
+package com.braingroom.user.viewmodel;
+
+/**
+ * Created by godara on 29/07/17.
+ */
+
+public class SplashViewModel extends ViewModel {
+}
